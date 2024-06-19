24523a115186332e81cad484e25fca9817b6a358
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-19 19:31:43 +0530

    Create firebase.dart

---
+++ firebase.dart
@@ -0,0 +1,13 @@
+firebase_core: ^2.24.0
+
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(/*Appname*/);
+}
