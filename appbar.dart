d5fff9a05d33c56d8ffdd647ff6a2ad89d5330af
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-20 09:46:39 +0530

    Create appbar.dart

---
+++ appbar.dart
@@ -0,0 +1,18 @@
+AppBar(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30.0),
+              bottomRight: Radius.circular(30.0),
+            ),
+          ),
+          leading: null,
+          actions: <Widget>[],
+          title: Text(
+            '                 AppBar',
+            style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+          ),
+          backgroundColor: Color(0xff237ACC),
+        ),
